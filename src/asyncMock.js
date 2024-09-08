11bab1e39027896b2e53a74793883d5c78a0b885
Franco Pignataro <francopignataro12@gmail.com>
2024-09-08 13:01:06 -0300

    tercer arreglo falta css

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -1,27 +1,76 @@
 const products = [
     {
         id: '1',
-        name: '',
-        price: 1000,
-        img:'',
-        stock: 10,
-        descripcion: ''
+        name: 'Fija',
+        price: 1050,
+        img:'https://i.ibb.co/dmVYQrB/fija.png',
+        stock: 9,
+        description: 'Suspensiones a medida a gusto del cliente'
     },
     {
         id: '2',
-        name: '',
-        price: 2000,
-        img:'',
-        stock: 10,
-        descripcion: ''
+        name: 'Regulable',
+        price: 1299,
+        img:'https://i.ibb.co/1mnYKxZ/regulable.png',
+        stock: 13,
+        description: 'Suspensiones con la capacidad de regular 10cm de altura'
     },
     {
         id: '3',
-        name: '',
-        price: 3000,
-        img:'',
-        stock: 10,
-        descripcion: ''
+        name: 'AirRide Black 8mm',
+        price: 4049,
+        img:'https://i.ibb.co/vhGgtMW/8mm2.png',
+        stock: 5,
+        description: 'La mejor suspension del mercado, con posibilidad de regular la altura instantaneamente'
+    },
+    {
+        id: '4',
+        name: 'AirRide Black 10mm',
+        price: 4799,
+        img:'https://i.ibb.co/5597xzP/8mm.png',
+        stock: 4,
+        description: 'La mejor suspension del mercado, con posibilidad de regular la altura instantaneamente'
+    },
+    {
+        id: '5',
+        name: 'AirRide Black 12mm',
+        price: 8230,
+        img:'https://i.ibb.co/XZ2gNgz/12mm.png',
+        stock: 6,
+        description: 'La mejor suspension del mercado, con posibilidad de regular la altura instantaneamente'
+    },
+    {
+        id: '6',
+        name: 'Compresor 160psi',
+        price: 799,
+        img:'https://i.ibb.co/PN8SM0M/compresor.png',
+        stock: 22,
+        description: 'Compresor Suspension Neumatica de 160psi'
+    },
+    {
+        id: '7',
+        name: 'Tanque de aluminio 11L',
+        price: 960,
+        img:'https://i.ibb.co/3NfBS9x/tanque.png',
+        stock: 33,
+        description: 'Tanque de aluminio para tu Suspension Neumatica, con capacidad de 11L'
+    },
+
+    {
+        id: '8',
+        name: 'Bolsa de aire 6.0',
+        price: 310,
+        img:'https://i.ibb.co/7YX7K06/bolsa.png',
+        stock: 30,
+        description: 'Bolsa de aire para tu Suspension Neumatica, de la mejor calidad!'
+    },
+    {
+        id: '9',
+        name: 'Control Castor AirRide',
+        price: 195,
+        img:'https://i.ibb.co/wSkPd3N/control.png',
+        stock: 110,
+        description: 'Control para tu suspension neumatica, compatible con todos los modulos'
     },
 ]
 
